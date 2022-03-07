33ae8e49b2daab8dea3639004d4750eba17bffed
tunahandündar <tunahan.dundar.2221@gmail.com>
2022-03-07 14:02:17 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import { render } from '@testing-library/react';
+import React, { Component } from "react";
+import './App.css'
+
+class App extends Component {
+  render() {
+    return <div class = "app-root">
+
+    </div>;
+  }
+}
+
+export default App;
