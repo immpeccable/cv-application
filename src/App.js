632ac72aae3f6af77fcf6f7eed9235bf461ce251
tunahandündar <tunahan.dundar.2221@gmail.com>
2022-03-09 15:56:19 +0300

    unnecessary files deleted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 
-import { render } from '@testing-library/react';
 import React, { Component } from "react";
 import './App.css'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import GeneralInfo from './components/GeneralInfo';
-import Education from './components/Education';
-import WorkingExp from './components/WorkingExp';
 
 class App extends Component {
 
